743e39328acb743faeb0d241a68012b3eb30034b
Krzysztof Kasowicz <krzysztofkasowicz1995@gmail.com>
2018-04-05 23:11:36 +0200

    05.04.2018 Added initial template to generateButton, fix clear button (button now can reset check boxes)

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -7,6 +7,17 @@ import javafx.scene.control.*;
 
 
 public class MainController {
+    private String beginning = "Poczatkowy kod";
+    private String capacityCode = "Capacity code";
+    private String hairTypeCode = "Hair type";
+    private String aboutCode = "About code";
+    private String end = "End";
+
+    private String beginningAllegro = "Poczatkowy kod Allegro";
+    private String capacityCodeAllegro = "Capacity code Allegro";
+    private String hairTypeCodeAllegro = "Hair type Allegro";
+    private String aboutCodeAllegro = "About code Allegro";
+    private String endAllegro = "End Allegro";
     @FXML
     private TextField auctionNameTextField;
 
@@ -66,17 +77,39 @@ public class MainController {
 
     @FXML
     void openClearButton(ActionEvent event) {
-        auctionNameTextField.setText("");
-        allegroAuctionNameTextField.setText("");
-        productTypeTextField.setText("");
-        capacityTextField.setText("");
-        hairTypeTextField.setText("");
-        aboutTextField.setText("");
-        descriptionTextField.setText("");
+        auctionNameTextField.clear();
+        allegroAuctionNameTextField.clear();
+        productTypeTextField.clear();
+        capacityTextField.clear();
+        hairTypeTextField.clear();
+        aboutTextField.clear();
+        descriptionTextField.clear();
+        auctionTextArea.clear();
+        allegroAuctionTextArea.clear();
+        aboutCheckBox.setSelected(false);
+        hairTypeCheckBox.setSelected(false);
+        capacityCheckBox.setSelected(false);
     }
 
     @FXML
     void openGenerateButton(ActionEvent event) {
-        auctionTextArea.setText(auctionNameTextField.getText());
+        auctionTextArea.setText(beginning + " " + auctionNameTextField.getText() + "\n" + descriptionTextField.getText() + "\n " + productTypeTextField.getText() + "\n");
+        if (capacityCheckBox.isSelected())
+            auctionTextArea.appendText(capacityCode + capacityTextField.getText() + "\n");
+        if (hairTypeCheckBox.isSelected())
+            auctionTextArea.appendText(hairTypeCode + hairTypeTextField.getText() + "\n");
+        if (aboutCheckBox.isSelected())
+            auctionTextArea.appendText(aboutCode + aboutTextField.getText() + "\n");
+        auctionTextArea.appendText(end);
+
+        allegroAuctionTextArea.setText(beginningAllegro + "\n" + allegroAuctionNameTextField.getText() + "\n" + descriptionTextField.getText() + "\n " + productTypeTextField.getText() + "\n");
+        if (capacityCheckBox.isSelected())
+            allegroAuctionTextArea.appendText(capacityCodeAllegro + capacityTextField.getText() + "\n");
+        if (hairTypeCheckBox.isSelected())
+            allegroAuctionTextArea.appendText(hairTypeCodeAllegro + hairTypeTextField.getText() + "\n");
+        if (aboutCheckBox.isSelected())
+            allegroAuctionTextArea.appendText(aboutCodeAllegro + aboutTextField.getText() + "\n");
+        allegroAuctionTextArea.appendText(endAllegro);
+
     }
 }
